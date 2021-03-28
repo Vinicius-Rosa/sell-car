f6ee5fb8a0388280f8eec844ddb3e5fc055b4f7c
vinicius-rosa <viniciusrosarafaeldeoliveira@gmail.com>
2021-03-28 10:03:08 -0300

    implement footer

--- src/components/Footer.component.vue
+++ src/components/Footer.component.vue
@@ -1,11 +1,25 @@
 <template>
   <footer>
-    <h3>
-      Developed with ❤ by
-      <a href="https://github.com/Vinicius-Rosa" target="_blank">
-        Vinicius Rosa
+    <div class="logo">
+      <router-link to="/">
+        <img src="../assets/logo.png" alt="" />
+      </router-link>
+    </div>
+    <div class="policies-terms">
+      <a href="#" class=""> Termos de uso </a>
+      <a href="#" class=""> Política de Privacidade </a>
+    </div>
+    <div class="medias">
+      <a href="#" class="media facebook">
+        <i class="fab fa-facebook-square"></i>
       </a>
-    </h3>
+      <a href="#" class="media instagram">
+        <i class="fab fa-instagram-square"></i>
+      </a>
+      <a href="#" class="media youtube">
+        <i class="fab fa-youtube"></i>
+      </a>
+    </div>
   </footer>
 </template>
 
@@ -19,13 +33,33 @@ export default {
 footer {
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 
   background-color: #000;
-  height: 80px;
+  min-height: 200px;
+
+  padding: 0 100px;
 }
 
 a {
-  color: pink;
+  cursor: pointer;
+  transition: 0.4s;
+}
+a:hover {
+  color: #d7af70;
+}
+
+.logo img {
+  max-height: 60px;
+}
+
+.policies-terms a {
+  display: block;
+  margin: 10px 0;
+}
+
+.medias a {
+  font-size: 2rem;
+  margin: 0 10px;
 }
 </style>
